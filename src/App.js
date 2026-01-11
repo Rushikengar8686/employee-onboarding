252907f25d88f8a151ee346c99f6c64631751a10
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-11 13:44:41 +0530

    All Pages Route Created...!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import Layout from "./components/Layout";
+import Dashboard from "./Pages/Dashboard";
+import Companies from "./Pages/Companies";
+import Employees from "./Pages/Employees";
+import AddEmployee from "./Pages/AddEmployee";
+import CompanyDetails from "./Pages/CompanyDetails";
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/company/:id" element={<CompanyDetails />} />
+        </Routes>
+      </Layout>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        theme="colored"
+      />
+    </BrowserRouter>
   );
 }
 
